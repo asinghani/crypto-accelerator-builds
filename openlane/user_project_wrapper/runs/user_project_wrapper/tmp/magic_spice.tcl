c94aa32d4bbeb237fe4a023fbafb9a8723a5e663
Anish <anish_oss@anishsinghani.com>
2020-12-15 23:18:14 -0800

    Build user_project_wrapper

---
+++ openlane/user_project_wrapper/runs/user_project_wrapper/tmp/magic_spice.tcl
@@ -0,0 +1,24 @@
+
+lef read /home/anish/workspace/openlane/pdks/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
+if {  [info exist ::env(EXTRA_LEFS)] } {
+	set lefs_in $::env(EXTRA_LEFS)
+	foreach lef_file $lefs_in {
+		lef read $lef_file
+	}
+}
+def read /project/openlane/user_project_wrapper/runs/user_project_wrapper/results/routing/user_project_wrapper.def
+load user_project_wrapper -dereference
+cd /project/openlane/user_project_wrapper/runs/user_project_wrapper/results/magic/
+extract do local
+extract no capacitance
+extract no coupling
+extract no resistance
+extract no adjust
+# extract warn all
+extract
+
+ext2spice lvs
+ext2spice user_project_wrapper.ext
+feedback save /project/openlane/user_project_wrapper/runs/user_project_wrapper/logs/magic/magic_ext2spice.feedback.txt
+# exec cp user_project_wrapper.spice /project/openlane/user_project_wrapper/runs/user_project_wrapper/results/magic/user_project_wrapper.spice
+
